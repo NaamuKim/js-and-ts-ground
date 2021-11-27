1997eeee3d2c8a67be43bbc9b3ebbf77f4bc63e8
lost731 <lost731@naver.com>
2021-11-27 16:30:25 +0900

    Add: number-baseball-complete

--- typescript-fundamental/number-baseball/baseball.js
+++ typescript-fundamental/number-baseball/baseball.js
@@ -10,13 +10,14 @@ function chooseNumber() {
     }
 }
 chooseNumber();
-var result = document.createElement("h1");
+var result = document.createElement("h3");
 body.append(result);
 var form = document.createElement("form");
 document.body.append(form);
 var input = document.createElement('input');
 form.append(input);
 input.type = 'text';
+input.placeholder = "숫자 4개를 입력하세요!";
 input.maxLength = 4;
 var button = document.createElement('button');
 button.textContent = '입력';
@@ -37,5 +38,25 @@ form.addEventListener('submit', function (e) {
         var strike = 0;
         var ball = 0;
         wrongCount += 1;
+        if (wrongCount > 10) {
+            result.textContent = "10\uBC88 \uB118\uAC8C \uD2C0\uB824\uC11C \uC2E4\uD328! \uB2F5\uC740 ".concat(array.join(','), " \uC785\uB2C8\uB2E4!");
+            input.value = "";
+            input.focus();
+            chooseNumber();
+            wrongCount = 0;
+        }
+        else {
+            for (var i = 0; i < 4; i++) {
+                if (Number(answerArray[i]) === array[i]) {
+                    strike += 1;
+                }
+                else if (array.indexOf(Number(answerArray[i])) > -1) {
+                    ball += 1;
+                }
+            }
+            result.textContent = strike + '스트라이크' + ball + '볼입니다!';
+            input.value = '';
+            input.focus();
+        }
     }
 });
--- typescript-fundamental/number-baseball/baseball.ts
+++ typescript-fundamental/number-baseball/baseball.ts
@@ -13,19 +13,21 @@ function chooseNumber(){
 
 chooseNumber();
 
-const result=document.createElement("h1");
+const result=document.createElement("h3");
 body.append(result);
 const form= document.createElement("form");
 document.body.append(form);
 const input=document.createElement('input')
 form.append(input);
 input.type='text';
+input.placeholder="숫자 4개를 입력하세요!"
 input.maxLength=4;
 const button=document.createElement('button');
 button.textContent='입력';
 form.append(button);
 
 let wrongCount=0;
+
 form.addEventListener('submit',(e)=>{
     e.preventDefault();
     const answer=input.value;
@@ -40,5 +42,25 @@ form.addEventListener('submit',(e)=>{
         let strike=0;
         let ball=0;
         wrongCount+=1;
+        if(wrongCount > 10) {
+            result.textContent=`10번 넘게 틀려서 실패! 답은 ${array.join(',')} 입니다!`
+            input.value="";
+            input.focus();
+            chooseNumber();
+            wrongCount=0;
+        }
+        else{
+            for(let i=0;i<4;i++){
+                if(Number(answerArray[i])===array[i]) {
+                    strike+=1;
+                }
+                else if(array.indexOf(Number(answerArray[i]))>-1){
+                    ball+=1;
+                }
+            }
+            result.textContent=strike + '스트라이크' + ball + '볼입니다!';
+            input.value='';
+            input.focus();
+        }
     }
 })
