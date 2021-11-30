4a901e60a82ac9edb1c8dd8e4f74c3e4353a64cb
lost731 <lost731@naver.com>
2021-11-30 21:48:53 +0900

    Add: make-interface

---
+++ typescript-fundamental/rock-scissor-paper/index.js
@@ -0,0 +1,31 @@
+var imgCoords = "0";
+var rsp = {
+    ROCK: '0',
+    SCISSORS: '-142px',
+    PAPER: '-284px'
+};
+var score = {
+    Rock: 0,
+    SCISSORS: 1,
+    PAPER: -1
+};
+function computerChoice(imgCoords) {
+    return Object.keys(rsp).find(function (k) { return rsp[k] === imgCoords; });
+}
+document.querySelectorAll('.btn').forEach(function (btn) {
+    btn.addEventListener('click', function () {
+        var myChoice = this.textContent;
+        var myScore = score[myChoice];
+        var computer = score[computerChoice(imgCoords)];
+        var diff = myScore - computerScore;
+        if (diff === 0) {
+            console.log("비겼습니다.");
+        }
+        else if ([-1, 2].indexOf(diff)) {
+            console.log('이겼습니다');
+        }
+        else {
+            console.log('졌습니다');
+        }
+    });
+});
---
+++ typescript-fundamental/rock-scissor-paper/index.ts
@@ -0,0 +1,39 @@
+let imgCoords = "0";
+
+interface RSP {
+    readonly ROCK: '0',
+    readonly SCISSORS: '-142px',
+    readonly PAPER: '-284px',
+}
+
+const rsp: RSP = {
+    ROCK:'0',
+    SCISSORS:'-142px',
+    PAPER:'-284px',
+} as const;
+
+const score={
+    Rock: 0,
+    SCISSORS:1,
+    PAPER: -1
+} as const;
+
+function computerChoice(imgCoords: RSP[keyof RSP]): keyof RSP {
+    return Object.keys(rsp).find((k)=>rsp[k]===imgCoords);
+}
+
+document.querySelectorAll('.btn').forEach((btn)=>{
+    btn.addEventListener('click',function(){
+        const myChoice = this.textContent;
+        const myScore = score[myChoice];
+        const computer = score[computerChoice(imgCoords)];
+        const diff = myScore-computerScore;
+        if(diff===0){
+            console.log("비겼습니다.");
+        } else if([-1,2].indexOf(diff)) {
+            console.log('이겼습니다');
+        } else {
+            console.log('졌습니다');
+        }
+    })
+});
