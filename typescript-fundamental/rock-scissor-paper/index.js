d6b0db13c691008a4c775737a9ede5862763756e
lost731 <lost731@naver.com>
2021-12-01 23:42:29 +0900

    Add: Complete-RSP

--- typescript-fundamental/rock-scissor-paper/index.js
+++ typescript-fundamental/rock-scissor-paper/index.js
@@ -13,8 +13,12 @@ var score = {
 function computerChoice(imgCoords) {
     return Object.keys(rsp).find(function (k) { return rsp[k] === imgCoords; });
 }
+var interval;
+var point = 0;
 document.querySelectorAll('.btn').forEach(function (btn) {
     btn.addEventListener('click', function () {
+        clearInterval(interval);
+        setInterval(intervalMaker, 1000);
         var myChoice = this.textContent;
         var myScore = score[myChoice];
         var computerScore = score[computerChoice(imgCoords)];
@@ -22,11 +26,31 @@ document.querySelectorAll('.btn').forEach(function (btn) {
         if (diff === 0) {
             console.log("비겼습니다.");
         }
-        else if ([-1, 2].indexOf(diff)) {
+        else if ([-1, 2].includes(diff)) {
             console.log('이겼습니다');
+            point++;
         }
         else {
             console.log('졌습니다');
+            point--;
         }
+        document.querySelector('#point').textContent = "\uD604\uC7AC \uC810\uC218\uB294 ".concat(String(point), " \uC810\uC785\uB2C8\uB2E4.");
     });
 });
+intervalMaker();
+function intervalMaker() {
+    interval = setInterval(function () {
+        if (imgCoords === rsp.ROCK) {
+            imgCoords = rsp.SCISSORS;
+        }
+        else if (imgCoords === rsp.SCISSORS) {
+            imgCoords = rsp.PAPER;
+        }
+        else {
+            imgCoords = rsp.ROCK;
+        }
+        if (document.querySelector('#computer')) {
+            document.querySelector('#computer').style.background = "url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ".concat(imgCoords, " 0");
+        }
+    }, 100);
+}
--- typescript-fundamental/rock-scissor-paper/index.ts
+++ typescript-fundamental/rock-scissor-paper/index.ts
@@ -22,18 +22,45 @@ function computerChoice(imgCoords: RSP[keyof RSP]): keyof RSP {
     return (Object.keys(rsp) as ["ROCK","SCISSORS", "PAPER"]).find((k)=>rsp[k]===imgCoords)!;
 }
 
+let interval: number;
+let point: number=0;
 document.querySelectorAll('.btn').forEach((btn)=>{
     btn.addEventListener('click',function(this: HTMLButtonElement){
+        clearInterval(interval);
+        setInterval(intervalMaker,1000);
         const myChoice = this.textContent as keyof RSP;
         const myScore = score[myChoice];
         const computerScore = score[computerChoice(imgCoords)];
         const diff = myScore-computerScore;
         if(diff===0){
             console.log("비겼습니다.");
-        } else if([-1,2].indexOf(diff)) {
+        } else if([-1,2].includes(diff)) {
             console.log('이겼습니다');
+            point++;
         } else {
             console.log('졌습니다');
+            point--;
         }
+        (document.querySelector('#point') as HTMLDivElement)!.textContent=`현재 점수는 ${String(point)} 점입니다.`;
     })
 });
+
+
+intervalMaker();
+
+
+function intervalMaker(){
+    interval=setInterval(function () {
+            if (imgCoords === rsp.ROCK) {
+                imgCoords = rsp.SCISSORS;
+            } else if (imgCoords === rsp.SCISSORS) {
+                imgCoords = rsp.PAPER;
+            } else {
+                imgCoords = rsp.ROCK;
+            }
+            if (document.querySelector('#computer')) {
+                (document.querySelector('#computer') as HTMLDivElement).style.background = `url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ${imgCoords} 0`
+            }
+        },100
+    )
+}
