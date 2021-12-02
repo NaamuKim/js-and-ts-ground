43d12b3d514f3f930daa389ec9a4b24838ce2374
lost731 <lost731@naver.com>
2021-12-02 16:50:25 +0900

    Add: Hero and Sub class

--- typescript-fundamental/naamustone/stone.js
+++ typescript-fundamental/naamustone/stone.js
@@ -1,4 +1,25 @@
 "use strict";
+var Hero = /** @class */ (function () {
+    function Hero(mine) {
+        this.mine = mine;
+        this.att = Math.ceil(Math.random() * 2);
+        this.hp = Math.ceil(Math.random() * 5) + 25;
+        this.hero = true;
+        this.field = true;
+    }
+    return Hero;
+}());
+var Sub = /** @class */ (function () {
+    function Sub(mine) {
+        this.field = false;
+        this.mine = mine;
+        this.att = Math.ceil(Math.random() * 5);
+        this.hp = Math.ceil(Math.random() * 5);
+        this.cost = Math.floor((this.att + this.hp) / 2);
+    }
+    return Sub;
+}());
+new Hero(true);
 var opponent = {
     hero: document.getElementById('rival-hero'),
     deck: document.getElementById('rival-deck'),
--- typescript-fundamental/naamustone/stone.ts
+++ typescript-fundamental/naamustone/stone.ts
@@ -1,3 +1,42 @@
+interface Card {
+    att: number;
+    hp: number;
+    mine: boolean;
+    field: boolean
+    cost?: number;
+    hero?: boolean;
+}
+
+class Hero implements Card{
+    public att: number;
+    public hp: number;
+    private hero: boolean;
+    public mine: boolean;
+    private field: boolean;
+    constructor(mine:boolean) {
+        this.mine =mine;
+        this.att=Math.ceil(Math.random()*2);
+        this.hp=Math.ceil(Math.random()*5)+25;
+        this.hero=true;
+        this.field=true;
+    }
+}
+
+class Sub implements Card{
+    public att: number;
+    public hp: number;
+    public field: boolean= false;
+    public mine: boolean;
+    public cost: number;
+    constructor(mine:boolean) {
+        this.mine=mine;
+        this.att=Math.ceil(Math.random()*5);
+        this.hp =Math.ceil(Math.random()*5);
+        this.cost=Math.floor((this.att+ this.hp)/2);
+    }
+}
+new Hero(true);
+
 interface Player {
     hero: HTMLDivElement
     deck: HTMLDivElement
