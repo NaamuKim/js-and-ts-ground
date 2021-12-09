d42fa74e83e8c04916d620ac06ab48556b5e61ee
lost731 <lost731@naver.com>
2021-12-09 20:02:22 +0900

    Fix: jsx content

--- react-ts/practices/RSP.tsx
+++ react-ts/practices/RSP.tsx
@@ -22,15 +22,14 @@ const computerChoice = (imgCoords) => {
 const RSP =() =>{
     return (
         <>
-        <div id="computer" style={{background: `url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ${imgCoord}`}}>
+        <div id="computer" style={{background: `url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ${imgCoord} 0`}}>
             <div>
                 <button id="rock" className="btn" onClick={onClickBtn('바위')}>바위</button>
                 <button id="scissor" className="btn" onClick={onClickBtn('가위')}>가위</button>
                 <button id="paper" className="btn" onClick={onClickBtn('보')}>보</button>
-
             </div>
             <div>{result}</div>
-            <div>현재 점수는 {score}접입니다.</div>
+            <div>현재 점수는 {score}점입니다.</div>
         </div>
         </>
     )
