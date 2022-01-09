52e2a287e2a529ecd558faec8dd397a7e06362ba
lost731 <lost731@naver.com>
2022-01-09 22:14:33 +0900

    Add: star hovering and locking

--- rating-with-stars/src/js/components/rating/starPoint.js
+++ rating-with-stars/src/js/components/rating/starPoint.js
@@ -18,7 +18,7 @@ class StarPoint {
 		this.bindEvents();
 	}
 	//별점을 고정된 상태로 만들어 준다.
-	lockedStarPoint() {
+	lockStarPoint() {
 		this.lockedStarPoint = true;
 	}
 	unlockStarPoint() {
@@ -29,6 +29,7 @@ class StarPoint {
 	isLockedStarPoint() {
 		return this.lockedStarPoint;
 	}
+
 	bindEvents() {
 		//마우스 움직임 이벤트
 		this.starBackgroundElement.addEventListener("mousemove", (event) => {
@@ -42,8 +43,41 @@ class StarPoint {
 			if (this.isLockedStarPoint()) {
 				return;
 			}
-			console.log(isOverHalf);
+			this.renderStarPointImages({
+				drawableLimitIndex: starPointIndex,
+				isOverHalf,
+			});
 		});
+		this.starBackgroundElement.addEventListener("click", () =>
+			this.lockStarPoint()
+		);
+		this.starPointResetButton.addEventListener("click", () => {
+			this.unlockStarPoint();
+			this.resetStarPointImages();
+		});
+		this.starBackgroundElement.addEventListener("mouseout", () => {
+			!this.isLockedStarPoint() && this.resetStarPointImages();
+		});
+	}
+	renderStarPointImages(payload = {}) {
+		const { drawableLimitIndex = -1, isOverHalf = false } = payload;
+		Array.prototype.forEach.call(this.starimages, (starimage, index) => {
+			//현재 순환 순서보다 마우스가 호버된 별의 인덱스가 크다면 꽉찬변, 아니면 빈별
+			let imageSource =
+				index < drawableLimitIndex
+					? starImageSourceMap.full
+					: starImageSourceMap.empty;
+
+			if (drawableLimitIndex === index) {
+				imageSource = isOverHalf
+					? starImageSourceMap.full
+					: starImageSourceMap.half;
+			}
+			starimage.src = imageSource;
+		});
+	}
+	resetStarPointImages() {
+		this.renderStarPointImages();
 	}
 }
 
