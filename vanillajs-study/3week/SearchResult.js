20320d7872c5aca5acd3762b3f3527a5f600c418
Jiwon Kim <lost731@naver.com>
2022-02-17 14:48:10 +0900

    Fix: alt tag missing and request error handling

--- vanillajs-study/3week/SearchResult.js
+++ vanillajs-study/3week/SearchResult.js
@@ -1,18 +1,16 @@
 function SearchResult({ initialState, $target }) {
-  this.state = initialState
-  const $searchResultDiv = document.createElement('div')
-  $target.appendChild($searchResultDiv)
+  this.state = initialState;
+  const $searchResultDiv = document.createElement('div');
+  $target.appendChild($searchResultDiv);
 
   this.setState = (nextState) => {
-    this.state = nextState
-    this.render()
-  }
+    this.state = nextState;
+    this.render();
+  };
 
   this.render = () => {
-    const html = this.state
-      .map((data) => `<img src="${data.imageUrl}"/>`)
-      .join('')
-    $searchResultDiv.innerHTML = html
-  }
+    const html = this.state.map((data) => `<img alt="${data.title}" src="${data.imageUrl}"/>`).join('');
+    $searchResultDiv.innerHTML = html;
+  };
 }
-export default SearchResult
+export default SearchResult;
--- vanillajs-study/3week/ajax.js
+++ vanillajs-study/3week/ajax.js
@@ -1,9 +1,15 @@
-import { END_POINT } from './config.js'
+import { END_POINT } from './config.js';
 
 export const request = (url) => {
   return fetch(`${END_POINT}?${url}`)
-    .then((res) => res.json())
-    .catch((err) => {
-      console.log(err)
+    .then((res) => {
+      if (res.status !== 200) {
+        alert('요청이나 응답을 확인해주세요!');
+      } else {
+        return res.json();
+      }
     })
-}
+    .catch((err) => {
+      console.log(err);
+    });
+};
