f6bb4b1cd430791fe6f094f08b6a4b6439e7161e
Jiwon Kim <lost731@naver.com>
2022-10-16 23:52:49 +0900

    Add: 이벤트 루프 관련 추상화

---
+++ event-loop/추상화2.js
@@ -0,0 +1,30 @@
+const webAPI = {
+  eventQueue: [],
+  makeEvent: () => {
+    while (true) {
+      if (eventQueue.length) {
+        const event = eventQueue.shift();
+        try {
+          event();
+        } catch (err) {
+          reportError();
+        }
+      }
+    }
+  },
+};
+
+const syncEnv = {
+  callStack: [],
+  callWebAPI: (fn) => {
+    while (true) {
+      if (callStack.length) {
+        callStack.forEach((fn) => {
+          if (fn.type === 'async') {
+            webAPI.eventQueue.push(fn);
+          }
+        });
+      }
+    }
+  },
+};
